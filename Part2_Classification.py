41fc6162eff916b6ff8093f38d54887b0e431096
ira3 <insoorhee@hotmail.com>
2018-08-01 20:31:09 -0700

    TODO:Fixing the graph

--- Part2_Classification.py
+++ Part2_Classification.py
@@ -1,16 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
-sns.set(style="whitegrid")
-from scipy.stats import norm
 
-
-from scipy import stats
-from statsmodels.stats.multicomp import pairwise_tukeyhsd
-import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import Imputer, MinMaxScaler, StandardScaler
 from sklearn.naive_bayes import GaussianNB
@@ -19,9 +10,6 @@ from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 
 from sklearn.cluster import KMeans
-from sklearn.cluster import AgglomerativeClustering
-from sklearn.cluster import AffinityPropagation
-from sklearn.neighbors import LocalOutlierFactor
 from sklearn.decomposition import PCA 
 
 pd.set_option('display.max_rows', None)
@@ -29,30 +17,38 @@ pd.set_option('display.max_columns', None)
 
 OUTPUT_TEMPLATE = (
     'Daily\n'
+    'ones_daily:    {ones_daily}\n'
+    'zeros_daily:   {zeros_daily}\n'
     'Bayesian classifier: {bayes_daily:.3g}\n'
     'kNN classifier:      {knn_daily:.3g}\n'
-    'Standard SVM classifier:      {svm_daily_standard:.3g}\n'
-    'MinMax SVM classifier:      {svm_daily_minmax:.3g}\n'
+    'Standard SVM classifier:   {svm_daily_standard:.3g}\n'
+    'MinMax SVM classifier:     {svm_daily_minmax:.3g}\n'
     'PCA(2),clusters:\n'
     '{df_daily}\n'              
     'Monthly\n'
+    'ones_monthly:  {ones_monthly}\n'
+    'zeros_monthly: {zeros_monthly}\n'
     'Bayesian classifier: {bayes_monthly:.3g}\n'
     'kNN classifier:      {knn_monthly:.3g}\n'
-    'Standard SVM classifier:      {svm_monthly_standard:.3g}\n'
-    'MinMax SVM classifier:      {svm_monthly_minmax:.3g}\n'
+    'Standard SVM classifier:   {svm_monthly_standard:.3g}\n'
+    'MinMax SVM classifier:     {svm_monthly_minmax:.3g}\n'
     'PCA(2),clusters:\n'
     '{df_monthly}\n'        
     'Quarterly\n'
-    'Bayesian classifier: {bayes_quarterly:.3g}\n'
-    'kNN classifier:      {knn_quarterly:.3g}\n'
-    'Standard SVM classifier:      {svm_quarterly_standard:.3g}\n'
-    'MinMax SVM classifier:      {svm_quarterly_minmax:.3g}\n'
-        'PCA(2),clusters:\n'
+    'ones_quarterly:    {ones_quarterly}\n'
+    'zeros_quarterly:   {zeros_quarterly}\n'
+    'Bayesian classifier:   {bayes_quarterly:.3g}\n'
+    'kNN classifier:        {knn_quarterly:.3g}\n'
+    'Standard SVM classifier:   {svm_quarterly_standard:.3g}\n'
+    'MinMax SVM classifier:     {svm_quarterly_minmax:.3g}\n'
+    'PCA(2),clusters:\n'
     '{df_quarterly}\n'        
     'Yearly\n'
+    'ones_yearly:   {ones_yearly}\n'
+    'zeros_yearly:  {zeros_yearly}\n'
     'Bayesian classifier: {bayes_yearly:.3g}\n'
     'kNN classifier:      {knn_yearly:.3g}\n'
-    'Standard SVM classifier:      {svm_yearly_standard:.3g}\n'
+    'Standard SVM classifier:    {svm_yearly_standard:.3g}\n'
     'MinMax SVM classifier:      {svm_yearly_minmax:.3g}\n'
     'PCA(2),clusters:\n'
     '{df_yearly}\n'        
@@ -79,9 +75,6 @@ def get_clusters(X,no_clusters):
 def GNB_KN_SVC_SVC1 (X,y,n,no_clusters):
 
     X_train, X_test, y_train, y_test = train_test_split(X, y)
-
-    #sns.countplot(y, palette='hls').set_title(title + " Count")
-    #plt.show()
 
     GuassianNB_model = GaussianNB()
     GuassianNB_model.fit(X_train, y_train)
@@ -148,6 +141,8 @@ def main():
     plt.axis('tight')
     plt.xlim(-0.065, 0.065)
     plt.show()
+    ones_daily = len(data.loc[data['long_short'] == 1])
+    zeros_daily = len(data.loc[data['long_short'] == 0])
     
     filename = "Ultimate_Data" + "\\" + "Ultimate_Assortion_pct_Change_Monthly_Insoo.csv"
     monthly_data = pd.read_csv(filename, sep=',', encoding='utf-8')
@@ -161,6 +156,8 @@ def main():
     plt.axis('tight')
     plt.xlim(-0.125, 0.125)
     plt.show()
+    ones_monthly = len(monthly_data.loc[monthly_data['long_short'] == 1])
+    zeros_monthly = len(monthly_data.loc[monthly_data['long_short'] == 0])
 
     filename = "Ultimate_Data" + "\\" + "Ultimate_Assortion_pct_Change_Quarterly_Insoo.csv"
     quarterly_data = pd.read_csv(filename, sep=',', encoding='utf-8')
@@ -173,6 +170,8 @@ def main():
     plt.suptitle("Quarterly Distribution")
     plt.axis('tight')
     plt.show()
+    ones_quarterly = len(quarterly_data.loc[quarterly_data['long_short'] == 1])
+    zeros_quarterly = len(quarterly_data.loc[quarterly_data['long_short'] == 0])
 
 
     filename = "Ultimate_Data" + "\\" + "Ultimate_Assortion_pct_Change_Yearly_Insoo.csv"
@@ -186,24 +185,34 @@ def main():
     plt.suptitle("yearly Distribution")
     plt.axis('tight')
     plt.show()
+    ones_yearly = len(yearly_data.loc[yearly_data['long_short'] == 1])
+    zeros_yearly = len(yearly_data.loc[yearly_data['long_short'] == 0])
 
 
     print(OUTPUT_TEMPLATE.format(
+        ones_daily=ones_daily,
+        zeros_daily=zeros_daily,
         bayes_daily=GuassianNB_model_daily,
         knn_daily=KNeighbor_model_daily,
         svm_daily_standard=SVC_model_pipline_daily_standard,
         svm_daily_minmax=SVC_model_pipline_daily_minmax,
         df_daily=df_daily,
+        ones_monthly=ones_monthly,
+        zeros_monthly=zeros_monthly,
         bayes_monthly=GuassianNB_model_monthly,
         knn_monthly=KNeighbor_model_monthly,
         svm_monthly_standard=SVC_model_pipline_monthly_standard,
         svm_monthly_minmax=SVC_model_pipline_monthly_minmax,
         df_monthly=df_monthly,
+        ones_quarterly=ones_quarterly,
+        zeros_quarterly=zeros_quarterly,
         bayes_quarterly=GuassianNB_model_quarterly,
         knn_quarterly=KNeighbor_model_quarterly,
         svm_quarterly_standard=SVC_model_pipline_quarterly_standard,
         svm_quarterly_minmax=SVC_model_pipline_quarterly_minmax,
         df_quarterly=df_quarterly,
+        ones_yearly=ones_yearly,
+        zeros_yearly=zeros_yearly,
         bayes_yearly=GuassianNB_model_yearly,
         knn_yearly=KNeighbor_model_yearly,
         svm_yearly_standard=SVC_model_pipline_yearly_standard,
